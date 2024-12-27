45b2513b70ca1bbc38a59d34f401eab1251a5f2b
Varun M S <testvagrant@TestVagrants-MacBook-Pro-3.local>
2024-12-27 15:33:28 +0530

    Add Constants

---
+++ constants/Constants.js
@@ -0,0 +1,48 @@
+class Constants {
+}
+
+const TitleTexts = Object.freeze({
+    MEN_TSHIRTS_TITLE_TEXT:  'Men - Tshirts Products',
+});
+
+const Categories = Object.freeze({
+    MEN: 'MEN',
+    MEN_TSHIRTS: 'TSHIRTS',
+    MEN_JEANS: 'JEANS',
+    WOMEN: 'WOMEN',
+    WOMEN_DRESS: 'DRESS',
+    WOMEN_TOP: 'TOP',
+    WOMEN_SAREE: 'SAREE',
+    KIDS: 'KIDS',
+    KIDS_DRESS: 'DRESS',
+    KIDS_TOP_AND_SHIRTS: 'TOP & SHIRTS',
+});
+
+const Products = Object.freeze({
+    PURE_COTTON_NEON_GREEN_TSHIRT: 'Pure Cotton Neon Green Tshirt',
+});
+
+const ApiList = Object.freeze({
+    API_1_GET_ALL_PRODUCTS_LIST: 'API 1: Get All Products List',
+    API_2_POST_TO_ALL_PRODUCTS_LIST: 'API 2: POST To All Products List',
+    API_3_GET_ALL_BRANDS_LIST: 'API 3: Get All Brands List',
+    API_4_PUT_TO_ALL_BRANDS_LIST: 'API 4: PUT To All Brands List',
+    API_5_POST_TO_SEARCH_PRODUCT: 'API 5: POST To Search Product',
+    API_6_POST_TO_SEARCH_PRODUCT_WITHOUT_PARAM: 'API 6: POST To Search Product without search_product parameter',
+    API_7_POST_TO_VERIFY_LOGIN_VALID: 'API 7: POST To Verify Login with valid details',
+    API_8_POST_TO_VERIFY_LOGIN_WITHOUT_EMAIL: 'API 8: POST To Verify Login without email parameter',
+    API_9_DELETE_TO_VERIFY_LOGIN: 'API 9: DELETE To Verify Login',
+    API_10_POST_TO_VERIFY_LOGIN_INVALID: 'API 10: POST To Verify Login with invalid details',
+    API_11_POST_TO_CREATE_USER_ACCOUNT: 'API 11: POST To Create/Register User Account',
+    API_12_DELETE_TO_DELETE_USER_ACCOUNT: 'API 12: DELETE METHOD To Delete User Account',
+    API_13_PUT_TO_UPDATE_USER_ACCOUNT: 'API 13: PUT METHOD To Update User Account',
+    API_14_GET_USER_ACCOUNT_DETAIL: 'API 14: GET user account detail by email'
+});
+
+module.exports = {
+    Constants,
+    Categories,
+    Products,
+    ApiList,
+    TitleTexts,
+};
